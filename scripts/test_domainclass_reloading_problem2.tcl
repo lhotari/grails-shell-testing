bcf7b1d44940520fb9d6c62647a072101148f9d1
Lari Hotari <lari.hotari@sagire.fi>
2013-11-05 16:12:41 +0200

    Use 3.6.10.3-SNAPSHOT plugin version

--- scripts/test_domainclass_reloading_problem2.tcl
+++ scripts/test_domainclass_reloading_problem2.tcl
@@ -1,6 +1,9 @@
 #!/usr/bin/expect
 set timeout 60
 
+proc updateplugin {filename pluginname version} {
+    exec perl -i -p -e "s/(\[\'\"\]:$pluginname:)\[^'\"\]+(\[\'\"\])/\$\{1\}$version\$\{2\}/" $filename
+}
 proc addfield {filename fieldname} {
     puts "Adding field $fieldname to $filename"
     exec perl -i -p -e "s/^\}\$/String $fieldname\\n\}\\n/" $filename 
@@ -11,6 +14,7 @@ spawn bash --norc --noprofile
 expect "$ "
 send "grails -plain-output create-app dcreloadingapp\r"
 expect "$ "
+updateplugin "dcreloadingapp/grails-app/conf/BuildConfig.groovy" "hibernate" {3.6.10.3-SNAPSHOT}
 send "cd dcreloadingapp\r"
 expect "$ "
 send "grails -plain-output\r"
