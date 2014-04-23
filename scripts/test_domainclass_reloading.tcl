6953f9a18ae8bb7fc4deb791a56e343e6c3ef152
Lari Hotari <lhotari@gopivotal.com>
2014-04-23 10:08:21 +0300

    tweak script and wait for unit test to be created before continuing

--- scripts/test_domainclass_reloading.tcl
+++ scripts/test_domainclass_reloading.tcl
@@ -16,16 +16,19 @@ expect "grails>"
 send "run-app\r"
 expect "grails>"
 send "create-domain-class A\r"
+expect "|Created file test/unit/dcreloadingapp/ASpec.groovy"
+after 1000
 expect "grails>"
 send "create-scaffold-controller dcreloadingapp.A\r"
-expect "|Created file test/unit/dcreloadingapp/ASpec.groovy"
+expect "|Created file test/unit/dcreloadingapp/AControllerSpec.groovy"
 after 1000
 set timeout 10
 expect "\n"
 addfield "dcreloadingapp/grails-app/domain/dcreloadingapp/A.groovy" "name"
 expect "\n"
 for {set i 0} {$i < 10} {incr i} {
-    addfield "dcreloadingapp/grails-app/domain/dcreloadingapp/A.groovy" "field$i"    
+    addfield "dcreloadingapp/grails-app/domain/dcreloadingapp/A.groovy" "field$i"
+    after 300    
     expect "\n"
 }
 send "exit\r"
