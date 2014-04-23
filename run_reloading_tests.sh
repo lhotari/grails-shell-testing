6a0ad20ef7820a5d9dd1ba739624b6bdefd2d182
Lari Hotari <lhotari@gopivotal.com>
2014-04-23 10:18:25 +0300

    add script for running reloading tests

---
+++ run_reloading_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xe
+cd scripts
+./test_domainclass_reloading.tcl
+./test_domainclass_reloading_problem2.tcl
+./test_domainclass_reloading_scenario3.tcl
+./test_domainclass_reloading_scenario3_delayed.tcl
